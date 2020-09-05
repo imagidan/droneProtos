e9d28cd0c19b93df8050c756264289103d2ef608
Daniel <danaiedaniel@gmail.com>
2020-09-05 18:48:02 -0400

    Update proto1.ino

--- proto1/proto1.ino
+++ proto1/proto1.ino
@@ -1,37 +1,114 @@
-#include <Servo.h>
+/*
+Code taken from: https://gist.github.com/vitorleal/9083802 
+*/
 
+#include <Servo.h>
+ 
+//Create the 4 motors objects
 Servo m1;
 Servo m2;
 Servo m3;
 Servo m4;
-
+ 
+//Esc pins
 int pinM1 = 5;
 int pinM2 = 6;
 int pinM3 = 9;
 int pinM4 = 10;
 
-void setup(){
+//Min and max pulse
+int minPulseRate = 1000;
+int maxPulseRate = 2000;
+int throttleChangeDelay = 50;
+ 
+//SETUP
+void setup() {
   Serial.begin(9600);
+  Serial.setTimeout(500);
   
-  m1.attach(pinM1);
-  //m2.attach(pinM2);
-  //m3.attach(pinM3);
-  //m4.attach(pinM4);
-  delay(1);
-  //m1.write(10);
-  //m2.write(10);
-  //m3.write(10);
-  //m4.write(10);
-  delay(5000);
+  //Init escs
+  initEscs();
+}
+ 
+//LOOP
+void loop() {
+  // Wait for some input
+  if (Serial.available() > 0) {
+    
+    // Read the new throttle value
+    int throttle = normalizeThrottle(Serial.parseInt());
+    
+    // Print it out
+    Serial.print("Setting throttle to: ");
+    Serial.println(throttle);
+    
+    // Change throttle to the new value
+    changeThrottle(throttle);
+  }
+ 
+}
+ 
+//Change throttle value
+void changeThrottle(int throttle) {
+  int currentThrottle = readThrottle();
+  
+  int step = 1;
+  if(throttle < currentThrottle) {
+    step = -1;
+  }
+  
+  // Slowly move to the new throttle value 
+  while(currentThrottle != throttle) {
+    writeTo4Motors(currentThrottle + step);
+    
+    currentThrottle = readThrottle();
+    delay(throttleChangeDelay);
+  }
 }
 
-void loop(){
-  while (Serial.available() > 0){
-    int val = Serial.parseInt();
-    Serial.println(val);
-    m1.write(val);
-    //m2.write(val);
-    //m3.write(val);
-    //m4.write(val);  
-  }  
+//Read the throttle value
+int readThrottle() {
+  int throttle = m1.read();
+  
+  Serial.print("Current throttle is: ");
+  Serial.print(throttle);
+  Serial.println();
+  
+  return throttle;
+}
+
+//Change velocity of the 4 escs at the same time
+void writeTo4Motors(int throttle) {
+  m1.write(throttle);
+  m2.write(throttle);
+  m3.write(throttle);
+  m4.write(throttle);
+}
+
+//Init escs
+void initEscs() {
+  m1.attach(pinM1, minPulseRate, maxPulseRate);
+  m2.attach(pinM2, minPulseRate, maxPulseRate);
+  m3.attach(pinM3, minPulseRate, maxPulseRate);
+  m4.attach(pinM4, minPulseRate, maxPulseRate);
+  
+  //Init motors with 0 value
+  writeTo4Motors(0);
+}
+
+//Start the motors
+void startUpMotors() {
+  writeTo4Motors(50);
+}
+ 
+// Ensure the throttle value is between 0 - 180
+int normalizeThrottle(int value) {
+  if(value < 0) {
+    return 0;
+    
+  } else if(value > 180) {
+    return 180;
+  }
+  
+  return value;
 }
